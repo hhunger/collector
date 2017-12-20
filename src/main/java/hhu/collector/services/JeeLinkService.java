3390f2254c5282006cf006e2d8263de1709ef36f
Henning Hunger <henning.hunger@gmail.com>
2017-12-20 08:34:40 +0100

    Add some test data for JeeLink

---
+++ src/main/java/hhu/collector/services/JeeLinkService.java
@@ -0,0 +1,7 @@
+package hhu.collector.services;
+
+public class JeeLinkService {
+//    JeeLink message format:
+//    OK 9 48 1 4 222 59
+//    OK "Always 9" "Sensor ID" "Temp * 10" "Tempo * 1" "Temp * 0.1" "Humidity + Weak Battery Flag"
+}
